e2884b9a2678cc831b2454c32e75cfb8a2069679
alexanderdilthey@outlook.com <alexanderdilthey@outlook.com>
2017-07-05 16:44:34 -0400

    Perl scripts for DB

---
+++ annotateRefSeqSequencesWithUniqueTaxonIDs.pl
@@ -0,0 +1,321 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use Data::Dumper;
+use Getopt::Long;
+use FindBin;
+use File::Find;
+use lib "$FindBin::Bin/perlLib";
+
+use taxTree;
+
+my $refSeqDirectory;
+my $taxonomyInDirectory;
+my $taxonomyOutDirectory;
+GetOptions (
+	'refSeqDirectory:s' => \$refSeqDirectory, 
+	'taxonomyInDirectory:s' => \$taxonomyInDirectory, 
+	'taxonomyOutDirectory:s' => \$taxonomyOutDirectory, 
+);
+
+unless($refSeqDirectory and $taxonomyInDirectory and $taxonomyOutDirectory)
+{
+	print_help();
+}
+
+die "Please specify valid directory for --refSeqDirectory" unless(-d $refSeqDirectory);
+die "Please specify valid directory for --taxonomyInDirectory" unless(-d $taxonomyInDirectory);
+
+
+print "Reading taxonomy from $taxonomyInDirectory ..\n";
+my $taxonomy_href = taxTree::readTaxonomy($taxonomyInDirectory);
+print "\tdone.\n\n";
+
+my @expected_taxonomy_files = taxTree::getTaxonomyFileNames();
+unless(-d $taxonomyOutDirectory)
+{
+	mkdir($taxonomyOutDirectory) or die "Can't mkdir $taxonomyOutDirectory (specified via --taxonomyOutDirectory)";
+}
+foreach my $f (@expected_taxonomy_files)
+{
+	my $cmd_cp_f = qq(cp ${taxonomyInDirectory}/${f} ${taxonomyOutDirectory}/);
+	system($cmd_cp_f) and die "Cannot execute cp command:\n$cmd_cp_f";
+}
+
+my %file_2_taxonID;
+my %file_2_organismName;
+my %taxonID_2_files;
+
+my %refSeq_categories;
+my %assemblyLevels;
+my %category_level_combined;
+my $included_genome = 0;
+my $missed_representative = 0;
+my $missed_referenceGenome = 0;
+my $missed_reference_and_representative = 0;
+print "Scanning $refSeqDirectory for *_assembly_report.txt\n";
+find(\&wanted, $refSeqDirectory);
+sub wanted {
+	my $file = $File::Find::name; 
+	{	
+		no warnings;
+		$file = $File::Find::name; 
+	}
+	
+	if((not -d $file) and ($file =~ /_assembly_report\.txt$/))
+	{	
+		remove_existing_fnaFile_for_assemblyReportFile($file);
+		
+		my $taxonID;
+		my $organismName = 'NA';
+		my $refSeq_category = '';
+		my $assembly_level = '';
+		open(REPORT, '<', $file) or die "Cannot open assembly report $file";
+		while(<REPORT>)
+		{
+			if($_ =~ /^# Taxid:\s*(\d+)/)
+			{
+				$taxonID = $1;
+			}
+			
+			if($_ =~ /^# Organism name:\s*(.+)/)
+			{
+				$organismName = $1;
+				$organismName =~ s/[\n\r]//g;
+			}				
+			
+			if($_ =~ /^# RefSeq category:\s*(.+)/)
+			{
+				$refSeq_category = $1;
+				$refSeq_category =~ s/[\n\r]//g;
+			}				
+			
+			if($_ =~ /^# Assembly level:\s*(.+)/)
+			{
+				$assembly_level = $1;
+				$assembly_level =~ s/[\n\r]//g;
+			}				
+							
+		}
+		close(REPORT);
+		unless(defined $taxonID)
+		{
+			die "Assembly report $file has not taxon ID";
+		}
+		
+		$refSeq_categories{$refSeq_category}++;
+		$assemblyLevels{$assembly_level}++;
+		$category_level_combined{join('_', $refSeq_category, $assembly_level)}++;
+
+		$included_genome++;
+		
+		if(exists $taxonomy_href->{$taxonID})
+		{
+			$file_2_taxonID{$file} = $taxonID;
+			$file_2_organismName{$file} = $organismName;
+			push(@{$taxonID_2_files{$taxonID}}, $file);			
+		}
+		else
+		{
+			die "Taxon ID $taxonID not defined in tree in $taxonomyInDirectory -- update your taxonomy directory?";
+		}
+			
+		# if(1) #  or $assembly_level eq 'Complete Genome'
+		# {
+
+		# }
+		# else
+		# {	
+			# die Dumper("Weird assembly_level", $assembly_level) unless(($assembly_level eq 'Contig') or ($assembly_level eq 'Scaffold') or ($assembly_level eq 'Chromosome'));
+			# if(($refSeq_category eq 'Representative Genome') and ($refSeq_category eq 'Reference Genome'))
+			# {
+				# $missed_reference_and_representative++;
+			# }
+			# elsif($refSeq_category eq 'Representative Genome')
+			# {
+				# $missed_representative++;
+			# }
+			# elsif($refSeq_category eq 'Reference Genome')
+			# {
+				# $missed_referenceGenome++;
+			# }
+			# else
+			# {
+				# die Dumper("Weird refSeq_category", $refSeq_category) unless(($refSeq_category eq '') or ($refSeq_category eq 'Representative Genome'));
+			# }
+		# }
+	}
+}
+
+print "Summary input data:\n";
+print "\tRefseq categories:\n";
+foreach my $key (keys %refSeq_categories)
+{
+	print "\t\t", $key, ": ", $refSeq_categories{$key}, "\n";
+}
+print "\tAssembly levels:\n";
+foreach my $key (keys %assemblyLevels)
+{
+	print "\t\t", $key, ": ", $assemblyLevels{$key}, "\n";
+}
+print "\t{Category} X {Assembly level}:\n";
+foreach my $key (keys %category_level_combined)
+{
+	print "\t\t", $key, ": ", $category_level_combined{$key}, "\n";
+}
+
+print "Total genomes: $included_genome \n";
+
+# print "\tMissed reference and representative: $missed_reference_and_representative \n";
+# print "\tMissed representative: $missed_representative \n";
+# print "\tMissed reference: $missed_referenceGenome \n";
+
+my $running_new_ids = 0;
+
+my %new_tree_relationships;
+my %new_tree_names;
+my %need_taxonID = (1 => 1);
+foreach my $taxonID (keys %taxonID_2_files)
+{
+	unless(exists $taxonomy_href->{$taxonID})
+	{
+		die "Taxon ID $taxonID not defined in tree in $taxonomyInDirectory -- update your taxonomy directory?";
+	}
+	
+	my $current_id = $taxonID;
+	do {
+		$need_taxonID{$current_id} = 1;
+		die unless(defined $taxonomy_href->{$current_id});	
+		$current_id = $taxonomy_href->{$current_id}[0];
+	} while($current_id != 1);	
+
+	if(scalar(@{$taxonID_2_files{$taxonID}}) > 1)
+	{
+		my $thisNode_rank = $taxonomy_href->{$taxonomy_href, $taxonID}{rank};
+		die unless(defined $thisNode_rank);
+		unless(($thisNode_rank eq 'species') or ($thisNode_rank eq 'no rank') or ($thisNode_rank eq 'subspecies'))
+		{
+			die Dumper("Unexpected rank", $thisNode_rank, $taxonID, $taxonID_2_files{$taxonID});
+		}	
+		my @associated_files = @{$taxonID_2_files{$taxonID}};
+		foreach my $f (@associated_files)
+		{
+			$running_new_ids++; 
+			my $newID = 'x' . $running_new_ids;
+			$file_2_taxonID{$f} = $newID;
+			$new_tree_relationships{$newID} = $taxonID;
+			$new_tree_names{$newID} = $file_2_organismName{$f};
+			$taxonID_2_files{$newID} = [$f];
+		}
+		
+		delete $taxonID_2_files{$taxonID};
+	}
+}
+
+print "Introduced $running_new_ids new taxonomic IDs\n";
+
+my $contigCounter = 0;
+foreach my $assemblyReportFile (keys %file_2_taxonID)
+{
+	my $taxonID = $file_2_taxonID{$assemblyReportFile};
+	my $fresh_fnaFile = get_fresh_fnaFile_for_assemblyReportFile($assemblyReportFile);
+	my $f2 = $fresh_fnaFile . '_';
+	open(F, '<', $fresh_fnaFile) or die "Cannot open $fresh_fnaFile";
+	open(F2, '>', $f2) or die "Cannot open $f2";
+	while(<F>)
+	{
+		my $line = $_;
+		if(substr($line, 0, 1) eq '>')
+		{
+			substr($line, 0, 1) = '';
+			die "File $fresh_fnaFile already conains kraken segment?" if ($line =~ /kraken:taxid\|(\d+)/);
+			$contigCounter++;
+			my $newID = 'C' . $contigCounter . '|kraken:taxid|' . $taxonID . '|' . $line;
+			print F2 '>', $newID;
+		}
+		else
+		{
+			print F2 $line;
+		}
+	}
+	close(F2);
+	close(F);
+	
+	my $mv_command = qq(mv $f2 $fresh_fnaFile);
+	die "Command $mv_command failed" if (system($mv_command));
+}
+
+print "Annotated $contigCounter contigs\n";
+
+my $taxonomy_names_f_in = $taxonomyInDirectory . '/names.dmp';
+my $taxonomy_nodes_f_in = $taxonomyInDirectory . '/nodes.dmp';
+
+
+my $taxonomy_names_f_out = $taxonomyOutDirectory . '/names.dmp';
+my $taxonomy_nodes_f_out = $taxonomyOutDirectory . '/nodes.dmp';
+
+die unless(-e $taxonomy_names_f_out);
+open(NAMESOUT, '>>', $taxonomy_names_f_out) or die "Cannot open $taxonomy_names_f_out";
+foreach my $newID (keys %new_tree_relationships)
+{
+	print NAMESOUT join("\t|\t", $newID, $new_tree_names{$newID}, '', 'scientific name', ''), "\n";
+}
+close(NAMESOUT);
+
+die unless(-e $taxonomy_nodes_f_out);
+open(NODESOUT, '>>', $taxonomy_nodes_f_out) or die "Cannot open $taxonomy_nodes_f_out";
+foreach my $newID (keys %new_tree_relationships)
+{
+	print NODESOUT join("\t|\t", $newID,  $new_tree_relationships{$newID}, 'pseudospecies', ''), "\n";
+}	
+close(NODESOUT);
+
+print "\nOutput new taxonomy into $taxonomyOutDirectory\n\n";
+
+sub remove_existing_fnaFile_for_assemblyReportFile
+{
+	my $assemblyReport = shift;
+	die "Weird apparent assembly report file $assemblyReport" unless($assemblyReport =~ /_assembly_report\.txt$/);
+	
+	my $gzFile = $assemblyReport;
+	$gzFile =~ s/_assembly_report\.txt/_genomic.fna.gz/;
+	die "Expected file $gzFile  not existing" unless(-e $gzFile);
+	
+	my $fna_file = $gzFile;
+	$fna_file =~ s/\.gz$//;
+	if(-e $fna_file)
+	{
+		unlink($fna_file) or die "Cannot unlink $fna_file";
+	}			
+}
+
+
+sub print_help
+{
+	print qq(
+annotateRefSeqSequencesWithUniqueTaxonIDs.pl
+
+  Prepares a RefSeq/GenBank download for MetaMap.
+  
+Usage:
+
+  perl annotateRefSeqSequencesWithUniqueTaxonIDs.pl --refSeqDirectory DIR --taxonomyInDirectory DIR --taxonomyOutDirectory DIR
+  
+Details:
+
+  Reads a directory structure as downloaded from RefSeq / Genbank servers,
+  unzips assemblies, gets information from *_assembly_report files, and writes
+  FASTA files with taxonID information in FASTA IDs.
+  
+  Each sub-directory is treated as a separate 'mappping unit' that will receive a
+  unique mapping ID - the script will create pseudo taxonomic IDs, prefixed with an
+  'x', if necessary.
+  
+  The specified --refSeqDirectory is manipulated in-place, and the amended taxonomy
+  is written into --taxonomyOutDirectory\n\n
+  
+);
+ 
+exit; 
+}
---
+++ buildDB.pl
@@ -0,0 +1,183 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use Data::Dumper;
+use Getopt::Long;
+use File::Find;
+use FindBin;
+use lib "$FindBin::Bin/perlLib";
+use List::Util qw/shuffle/;
+use List::MoreUtils qw/all/;
+use Cwd qw/abs_path getcwd/;
+use taxTree;
+
+my $DB = 'refseq';
+my $FASTAs;
+my $taxonomyDir;
+
+GetOptions (
+	'DB:s' => \$DB, 
+	'FASTAs:s' => \$FASTAs, 
+	'taxonomy:s' => \$taxonomyDir, 
+);
+
+# Get input arguments
+
+unless($DB and $FASTAs and $taxonomyDir)
+{
+	print_help();
+}
+
+
+die "Please specify a taxonomy directory (parameter --taxonomy)" unless(-d $taxonomyDir);
+die "Taxonomy directory (--taxonomy) does not contain expected files for taxonomy in NCBI format (names.dmp, nodes.dmp..)." unless(all {-e $_} (map {$taxonomyDir . '/' . $_} taxTree::getTaxonomyFileNames()));
+
+unless(-d $DB)
+{
+	mkdir($DB) or die "Cannot mkdir $DB";
+}
+my @files_in_DB = glob($DB.'/*');
+die "Database output directory (parameter --DB) is not empty" unless(scalar(@files_in_DB) == 0);
+
+# Find input files
+
+my @FASTAfiles;
+if(-d $FASTAs)
+{
+	find(\&wanted, $FASTAs);
+	sub wanted {
+		my $file = $File::Find::name;
+		if($file =~ /(.fa$)|(\.fna$)/)
+		{
+			push(@FASTAfiles, $file);
+		}
+	}
+}
+else
+{
+	@FASTAfiles = split(/,/, $FASTAs);
+	foreach my $f (@FASTAfiles)
+	{
+		die "Specified FASTA file (via --FASTAs) doesn't exist: $f";
+	}
+}
+
+print "\nNumber of found FASTA input files: ", scalar(@FASTAfiles), "\n";
+
+# read taxonomy
+
+print "\nReading taxonomy from $taxonomyDir ..\n";
+my $taxonomy_href = taxTree::readTaxonomy($taxonomyDir);
+print "\tdone.\n\n";
+
+# get list of contigs IDs and their positions			
+					
+my %taxonIDs;
+my @contigs;
+for(my $fileI = 0; $fileI <= $#FASTAfiles; $fileI++)
+{
+	my $fileN = $FASTAfiles[$fileI];
+	open(F, '<', $fileN) or die "Cannot open file $fileN";
+	while(<F>)
+	{
+		if(substr($_, 0, 1) eq '>')
+		{
+			my $position_contig_start = tell(F) - length($_);			
+			chomp;
+			my $contigID = $_;
+			
+			push(@contigs, [$fileI, $position_contig_start, $contigID]);
+			
+			unless($contigID =~ /kraken:taxid\|(x?\d+)/)
+			{
+				die "Expect taxon ID in contig identifier - file $fileN - line $.";
+			}			
+			my $taxonID = $1;
+			unless(exists $taxonomy_href->{$taxonID})
+			{
+				die "Taxon ID $taxonID - from file $fileN, line $. - not found in taxonomy -- consider updating your taxonomy directory.";
+			}
+		}
+	}
+	close(F);
+}
+@contigs = shuffle @contigs;
+
+my $getContigSequence = sub {
+	my $contigInfoAref = shift;
+	
+	my $fn = $FASTAfiles[$contigInfoAref->[0]];
+	my $pos = $contigInfoAref->[1];
+	my $expectedName = $contigInfoAref->[2];
+	
+	open(F, '<', $fn) or die "Cannot open file $fn";
+	seek(F, $pos, 0);
+	my $firstLine = <F>;
+	chomp($firstLine);
+	die "Couldn't find contig - got $firstLine, wanted $expectedName, in $fn" unless($firstLine eq $expectedName);
+	my $contigSequence = $firstLine . "\n";
+	while(<F>)
+	{
+		if(substr($_, 0, 1) eq '>')
+		{
+			last;
+		}
+		else
+		{
+			$contigSequence .= $_;
+		}
+	}
+	close(F);
+	
+	return $contigSequence;
+};
+
+my $outputFN = $DB . '/' . 'DB.fa';
+open(DB, '>', $outputFN) or die "Cannot open $outputFN - check whether I have write permissions";
+for(my $contigI = 0; $contigI <= $#contigs; $contigI++)
+{
+	my $contigInfo = $contigs[$contigI];
+	my $contigSequence = $getContigSequence->($contigInfo);
+	print DB $contigSequence;
+}
+close(DB);
+
+print "\nProduced randomized-order database sequence file $outputFN\n\n";
+
+
+
+
+sub print_help
+{
+	print qq(
+buildDB.pl
+
+  Assemble a MetaMap database.
+  
+Usage:
+
+  perl buildDB.pl --DB dbNAME --FASTAs DIR|FILELIST --taxonomy DIR
+  
+Parameters:
+
+  DB
+      Name (output directory) of DB to be constructed.
+  
+  FASTAs
+  
+      Directory with or comma-separated list of FASTA input
+	  files. Directories are scanned recursively (*.fa, *.fna).
+      Each contig has to be annotated with a taxon ID
+      satisfying the regular expression 'kraken:taxid\\|(x?\\d+)'.
+      
+  taxonomy
+      
+      Path to taxonomy directory (in NCBI format, containing
+      files nodes.dmp, names.dmp etc.) - all taxon IDs present
+      in the FASTA must be present in the taxonomy, including
+      pseudo IDs starting with an 'x'.
+  
+);
+exit;
+}
